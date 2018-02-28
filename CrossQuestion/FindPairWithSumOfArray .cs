ad9351b45322d6e030e6d055d26d3600170fee28
Asad Muhammad <asad.muhammad@nxb.com.pk>
2018-02-28 17:25:21 +0500

    first commit

---
+++ CrossQuestion/FindPairWithSumOfArray .cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossQuestion
+{
+    class FindPairWithSumOfArray
+    {
+        static void Main(String[] args)
+        {
+            string res = "";
+
+            int _a_size = 0;
+            _a_size = Convert.ToInt32(Console.ReadLine());
+            int[] _a = new int[_a_size];
+            int _a_item;
+            for (int _a_i = 0; _a_i < _a_size; _a_i++)
+            {
+                _a_item = Convert.ToInt32(Console.ReadLine());
+                _a[_a_i] = _a_item;
+            }
+            int sum = Convert.ToInt32(Console.ReadLine());
+            res = FindIndexOfSum(_a, sum);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+
+        static string FindIndexOfSum(int[] _a, int sum)
+        {
+            string Index = "";
+            for (int i = 0; i < _a.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(Index))
+                {
+                    break;
+                }
+                for (int j = 1; j < _a.Length; j++)
+                {
+                    if (_a[i] + _a[j] == sum)
+                    {
+                        Index = i + " " + j;
+                        break;
+                    }
+                }
+            }
+            return Index;
+        }
+    }
+}
---
+++ CrossQuestion/TwinString.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossQuestion
+{
+    class TwinString
+    {
+        static string[] twins(string[] a, string[] b)
+        {
+            string[] tempArray = new string[a.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                char[] text = a[i].ToCharArray();
+                int legth = text.Length;
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if ((j % 2 == 0) && (j > 0))
+                    {
+                        j = j + 2;
+                    }
+                    if ((j + 2) <= legth - 1)
+                    {
+                        char temp = text[j];
+                        text[j] = text[j + 2];
+                        text[j + 2] = temp;
+                    }
+                }
+                tempArray[i] = new string(text);
+            }
+
+            string[] result = new string[b.Length];
+            for (int l = 0; l < b.Length; l++)
+            {
+                if (tempArray.Length - 1 >= l)
+                {
+                    if (tempArray[l] == b[l])
+                    {
+                        result[l] = "Yes";
+                    }
+                    else
+                    {
+                        result[l] = "No";
+                    }
+                }
+                else
+                {
+                    result[l] = "No";
+                }
+            }
+
+
+            return result;
+        }
+
+        // DO NOT MODIFY CODE BELOW THIS LINE!
+        static void Main(String[] args)
+        {
+            string[] res;
+
+            int _a_size = 0;
+            _a_size = Convert.ToInt32(Console.ReadLine());
+            string[] _a = new string[_a_size];
+            string _a_item;
+            for (int _a_i = 0; _a_i < _a_size; _a_i++)
+            {
+                _a_item = Console.ReadLine();
+                _a[_a_i] = _a_item;
+            }
+
+
+            int _b_size = 0;
+            _b_size = Convert.ToInt32(Console.ReadLine());
+            string[] _b = new string[_b_size];
+            string _b_item;
+            for (int _b_i = 0; _b_i < _b_size; _b_i++)
+            {
+                _b_item = Console.ReadLine();
+                _b[_b_i] = _b_item;
+            }
+
+            res = twins(_a, _b);
+            for (int res_i = 0; res_i < res.Length; res_i++)
+            {
+                Console.WriteLine(res[res_i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
